3b77514b0c24213530f155211ebe67be211b189f
J222 <baram789456@gmail.com>
2024-03-12 15:58:47 +0300

    [Style] remove unused code

--- src/main.tsx
+++ src/main.tsx
@@ -23,12 +23,10 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  // <React.StrictMode>
   <QueryProvider>
     <ThemeProvider>
       <RouterProvider router={router} />
       <Toaster />
     </ThemeProvider>
   </QueryProvider>,
-  // </React.StrictMode>,
 );
