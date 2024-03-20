2da1a64c3a27abbf2f075940e40cbf6705944aba
J222 <baram789456@gmail.com>
2024-03-21 01:42:47 +0300

    [Refactor] Remove sidecar

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,8 @@
-use std::sync::{Arc, Mutex};
 use std::process::Child;
 use tauri::{Manager, App, WindowEvent};
 use tauri::api::process::{Command, CommandEvent};
 
 fn main() {
-//     #[cfg(not(debug_assertions))] // only include this code on release builds
-//         let app_child_process = Arc::new(Mutex::new(None));
-
     tauri::Builder::default()
         .setup({
             move |app| {
@@ -21,24 +17,9 @@ fn main() {
                 {
                     let main_window = app.get_window("main").unwrap();
                     // Spawn the external process
-//                     let setup_child_process = app_child_process.clone();
-                    let (mut rx, child) = Command::new_sidecar("backend")
-                        .expect("failed to setup `backend` sidecar")
+                    let (mut rx, child) = Command::new("./backend")
                         .spawn()
-                        .expect("Failed to spawn backend process");
-
-                    // Store the child process handle
-//                     *setup_child_process.lock().unwrap() = Some(child);
-
-//                     tauri::async_runtime::spawn(async move {
-//                         while let Some(event) = rx.recv().await {
-//                             if let CommandEvent::Stdout(line) = event {
-//                                 main_window
-//                                     .emit("backend-message", &line)
-//                                     .expect("failed to emit backend message event");
-//                             }
-//                         }
-//                     });
+                        .expect("failed to setup `backend` sidecar");
                 }
                 Ok(())
             }
