36d6a36d82ef2904547f0a85cff1b77d9d8b9e14
J222 <baram789456@gmail.com>
2024-03-20 23:41:03 +0300

    [Refactor] Remove backend process handle

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,8 +4,8 @@ use tauri::{Manager, App, WindowEvent};
 use tauri::api::process::{Command, CommandEvent};
 
 fn main() {
-    #[cfg(not(debug_assertions))] // only include this code on release builds
-        let app_child_process = Arc::new(Mutex::new(None));
+//     #[cfg(not(debug_assertions))] // only include this code on release builds
+//         let app_child_process = Arc::new(Mutex::new(None));
 
     tauri::Builder::default()
         .setup({
@@ -21,24 +21,24 @@ fn main() {
                 {
                     let main_window = app.get_window("main").unwrap();
                     // Spawn the external process
-                    let setup_child_process = app_child_process.clone();
+//                     let setup_child_process = app_child_process.clone();
                     let (mut rx, child) = Command::new_sidecar("backend")
                         .expect("failed to setup `backend` sidecar")
                         .spawn()
                         .expect("Failed to spawn backend process");
 
                     // Store the child process handle
-                    *setup_child_process.lock().unwrap() = Some(child);
+//                     *setup_child_process.lock().unwrap() = Some(child);
 
-                    tauri::async_runtime::spawn(async move {
-                        while let Some(event) = rx.recv().await {
-                            if let CommandEvent::Stdout(line) = event {
-                                main_window
-                                    .emit("backend-message", &line)
-                                    .expect("failed to emit backend message event");
-                            }
-                        }
-                    });
+//                     tauri::async_runtime::spawn(async move {
+//                         while let Some(event) = rx.recv().await {
+//                             if let CommandEvent::Stdout(line) = event {
+//                                 main_window
+//                                     .emit("backend-message", &line)
+//                                     .expect("failed to emit backend message event");
+//                             }
+//                         }
+//                     });
                 }
                 Ok(())
             }
