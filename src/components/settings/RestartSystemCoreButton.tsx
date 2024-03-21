7d75265f1fe7c2c1b80f0b14983801221bd3fff6
J222 <baram789456@gmail.com>
2024-03-21 22:09:05 +0300

    [Fix] Restart core button

---
+++ src/components/settings/RestartSystemCoreButton.tsx
@@ -0,0 +1,43 @@
+import { useRestartBackend } from '@/hooks/useBackend.ts';
+import { toast } from '@/components/ui/use-toast.ts';
+import { Button } from '@/components/ui/button.tsx';
+import { Icons } from '@/components/common/Icons.tsx';
+import { LucidePower } from 'lucide-react';
+
+export default function RestartSystemCoreButton() {
+  const restartBackendMutation = useRestartBackend();
+
+  const handleRestart = () => {
+    if (confirm('Are you sure you want to restart the trading system core?')) {
+      restartBackendMutation.mutate();
+    }
+  };
+
+  if (restartBackendMutation.isError) {
+    toast({
+      title: `Error`,
+      description: 'Trading core restarted failed',
+      variant: 'destructive',
+    });
+  }
+
+  if (restartBackendMutation.isSuccess) {
+    toast({
+      title: `Success`,
+      description: 'Trading core restarted successfully',
+    });
+  }
+
+  return (
+    <Button disabled={restartBackendMutation.isPending} onClick={handleRestart} variant="outline" type="button">
+      {restartBackendMutation.isPending ? (
+        <Icons.spinner className="h-4 w-4 animate-spin" />
+      ) : (
+        <>
+          <LucidePower className="mr-2 h-4 w-4" />
+          <span>Restart trading system core</span>
+        </>
+      )}
+    </Button>
+  );
+}
--- src/components/settings/SystemSettings.tsx
+++ src/components/settings/SystemSettings.tsx
@@ -1,52 +1,12 @@
 import { Button } from '@/components/ui/button.tsx';
 import { Card, CardHeader, CardTitle, CardContent, CardDescription } from '@/components/ui/card.tsx';
-import { LucideMail, LucideSettings, LucidePower } from 'lucide-react';
+import { LucideMail, LucideSettings } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import VersionDisplay from '@/components/common/VersionDisplay.tsx';
 import { CONTACT_DEVELOPER_EMAIL } from '@/constants/developer.ts';
-import { useRestartBackend } from '@/hooks/useBackend.ts';
-import { Icons } from '@/components/common/Icons.tsx';
-import { toast } from '@/components/ui/use-toast.ts';
+import RestartSystemCoreButton from '@/components/settings/RestartSystemCoreButton.tsx';
 
 export default function SystemSettings() {
-  function RestartSystemCoreButton() {
-    const restartBackendMutation = useRestartBackend();
-
-    const handleRestart = () => {
-      if (confirm('Are you sure you want to restart the trading system core?')) {
-        restartBackendMutation.mutate();
-      }
-    };
-
-    if (restartBackendMutation.isError) {
-      toast({
-        title: `Error`,
-        description: 'Trading core restarted failed',
-        variant: 'destructive',
-      });
-    }
-
-    if (restartBackendMutation.isSuccess) {
-      toast({
-        title: `Success`,
-        description: 'Trading core restarted successfully',
-      });
-    }
-
-    return (
-      <Button disabled={restartBackendMutation.isPending} onClick={handleRestart} variant="outline" type="button">
-        {restartBackendMutation.isPending ? (
-          <Icons.spinner className="h-4 w-4 animate-spin" />
-        ) : (
-          <>
-            <LucidePower className="mr-2 h-4 w-4" />
-            <span>Restart trading system core</span>
-          </>
-        )}
-      </Button>
-    );
-  }
-
   return (
     <Card>
       <CardHeader>
@@ -56,7 +16,7 @@ export default function SystemSettings() {
         </CardDescription>
       </CardHeader>
       <CardContent className="grid grid-cols-1 gap-6">
-        {RestartSystemCoreButton()}
+        <RestartSystemCoreButton />
         <Button disabled variant="outline" type="button">
           <LucideSettings className="mr-2 h-4 w-4" />
           <span>Reset settings</span>
