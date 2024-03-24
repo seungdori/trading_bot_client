1ce9278693d0db1fd508fa7f0fffd52b50738c1a
J222 <baram789456@gmail.com>
2024-03-25 02:47:53 +0300

    [Refactor] Loading win rate table

--- src/components/winRate/WinRateTableWrapper.tsx
+++ src/components/winRate/WinRateTableWrapper.tsx
@@ -5,6 +5,7 @@ import { Icons } from '@/components/common/Icons.tsx';
 import WinRateTable from '@/components/winRate/WinRateTable.tsx';
 import ShowCoinChartButton from '@/components/winRate/ShowCoinChartButton.tsx';
 import { cn } from '@/lib/utils.ts';
+import { Button } from '@/components/ui/button.tsx';
 
 export default function WinRateTableWrapper({ exchange, className }: { exchange: Exchange; className?: string }) {
   const { isLoading, data } = useWinRates(exchange);
@@ -14,7 +15,12 @@ export default function WinRateTableWrapper({ exchange, className }: { exchange:
     <section className={cn('space-y-4', className)}>
       <WinRateTable exchange={exchange} assetsData={data ?? []} />
       {isLoading ? (
-        <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+        <Button disabled className="w-full">
+          <p className="flex flex-col justify-center items-center">
+            <Icons.spinner className="h-4 w-4 animate-spin" />
+            <span className="font-bold">AI 탐색 종목 로딩중....</span>
+          </p>
+        </Button>
       ) : (
         <ShowCoinChartButton exchange={exchange} />
       )}
