0ecab6a92ceb860c15c863c413e5113c12b33ed7
J222 <baram789456@gmail.com>
2024-03-27 13:14:53 +0300

    [Fix] Handle trading log websocket reconnect

--- src/hooks/useTransactionLog.ts
+++ src/hooks/useTransactionLog.ts
@@ -3,6 +3,7 @@ import { Exchange } from '@/types/exchangeTypes.ts';
 import { DESKTOP_BACKEND_WS_URL } from '@/schemas/backendSchema.ts';
 import { useTadingLogStore } from '@/store/transactionLogStore.ts';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
+import { toast } from '@/components/ui/use-toast.ts';
 
 export const useTradingLog = ({
   exchange,
@@ -14,7 +15,25 @@ export const useTradingLog = ({
   defaultMessage: string;
 }) => {
   const endpoint = new URL(`/trading/${exchange}/logs`, DESKTOP_BACKEND_WS_URL);
-  const { lastMessage, readyState } = useWebSocket(endpoint.href);
+  const { lastMessage, readyState } = useWebSocket(endpoint.href, {
+    share: true,
+    retryOnError: true,
+    reconnectAttempts: 20,
+    reconnectInterval: 5000,
+    onReconnectStop: (numAttempts: number) => {
+      console.error(`Reconnect stopped after [${numAttempts}] attempts`);
+      toast({
+        title: '로그 불러오기 실패',
+        description: `프로그램을 재시작 해주세요. Reconnect stopped after [${numAttempts}] attempts`,
+        duration: Infinity,
+        draggable: false,
+      });
+    },
+    shouldReconnect: (closeEvent) => {
+      console.log(closeEvent);
+      return true;
+    },
+  });
   const { logs, append, clearStorage } = useTadingLogStore(exchange);
   const [log, setLog] = useState<string>(logs?.join(delimiter) ?? defaultMessage);
 
